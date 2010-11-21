8f278108040bc11e3eeddb73af8e2d6537d00a13
Saket Saurabh <tachyon.saket@gmail.com>
2010-11-21 18:27:38 +0530

    adding sql.php again

--- login/sql.php
+++ login/sql.php
@@ -1,18 +1,9 @@
 <?php
-// $sqlhost = "localhost";
-// $sqluser = "itbhunet_acm";
-// $sqlpass = "iA,ctuH3m";
-// $db = "itbhunet_acm";
 
-//$sqlhost = "localhost";
-//$sqluser = "root";
-//$sqlpass = "saket";
-//$db = "acm";
-
-  $sqlhost = "10.3.31.50";
-  $sqluser = "acm";
-  $sqlpass = "1979tour";
-  $db = "acm";
+  $sqlhost = "";
+  $sqluser = "";
+  $sqlpass = "";
+  $db = "";
 
 
 ?>
